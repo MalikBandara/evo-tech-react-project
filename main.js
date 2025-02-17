733f8793267da20e802ac67cb2dbccf1754855ae
Malik <mmalith520@gmail.com>
2025-02-17 15:35:25 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// java script Comment
+
+//comment added inline comment
+
+/* this is multi line comment
+
+*/
+// js console log
+console.log("hear is myData1");
+
+// js variable declaration with var
+
+var name1;
+
+// scoped
+{
+  // ES6 (2015) variable declaration with let and const
+  // Scoped can be local or global
+  let name2;
+  // const variable declaration
+  const name3 = "Some Value";
+}
+
+function myFunction() {
+  let myVariable = 5000;
+  console.log(" Log 2  " + myVariable);
+}
+
+myFunction();
+
+// later Initialization the variable
+
+name1 = "Malik Bandara ";
+name2 = "Bandara";
+
+console.log(name1);
+console.log(name2);
+// console.log("LOg 1" + myVariable);
+
+
+
+
+
